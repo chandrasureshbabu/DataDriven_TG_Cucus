be04523113a1dc7546d93bc9b2557349dd9f9535
suresh <nemani.sureshbabu@gmail.com>
2023-02-25 11:54:17 +0530

    1st commit

---
+++ src/main/java/DataDriven/Datadriven_TG_Cucus/App.java
@@ -0,0 +1,13 @@
+package DataDriven.Datadriven_TG_Cucus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
