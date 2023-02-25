e821337f3d9582306475efb4b0ab04f59a952fb0
suresh <nemani.sureshbabu@gmail.com>
2023-02-25 15:45:44 +0530

    grid Issue fix only Check In

---
+++ src/test/java/DataDriven/Datadriven_TG_Cucus/Issues.java
@@ -0,0 +1,15 @@
+package DataDriven.Datadriven_TG_Cucus;
+
+import org.testng.annotations.Test;
+
+public class Issues {
+	
+	
+	
+	@Test
+	public void IssueGrid()
+	{
+		System.out.println("Handle grids");
+	}
+
+}
