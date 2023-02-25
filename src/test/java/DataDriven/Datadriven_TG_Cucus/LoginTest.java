12bb5f5d3e4d3c8450d23a49afaef0964e427083
suresh <nemani.sureshbabu@gmail.com>
2023-02-25 14:17:24 +0530

    Login form add

---
+++ src/test/java/DataDriven/Datadriven_TG_Cucus/LoginTest.java
@@ -0,0 +1,14 @@
+package DataDriven.Datadriven_TG_Cucus;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void Loginform()
+	{
+		System.out.println("Enter username");
+		System.out.println("Enter password");
+	}
+
+}
